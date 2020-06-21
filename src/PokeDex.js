12a80027be296a6eb7534c3e2b22063cc4e3b6a7
tanjimTC <tanjimtc74@gmail.com>
2020-06-21 13:52:06 +0600

    first commit

---
+++ src/PokeDex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokeDex = () => {
+    return (
+        <div>
+            <h1>Pokedex</h1>
+        </div>
+    );
+};
+
+export default PokeDex;
---
+++ src/Pokemon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokemon = () => {
+    return (
+        <div>
+            <h1>Pokemon</h1>
+        </div>
+    );
+};
+
+export default Pokemon;
