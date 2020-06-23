bdff6f2e12bd16c7690c130d19ea385a5c39456e
tanjimTC <tanjimtc74@gmail.com>
2020-06-23 22:06:38 +0600

    Updates

---
+++ precache-manifest.f1af5a2522632539583a7d8f2d6d29ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bbf627eae547bd74f2529619c1f6a8",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "6c4b8a0f7eef38937437",
+    "url": "/pokedex/static/js/2.111d7447.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.111d7447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb57083bf3ca8cd682b6",
+    "url": "/pokedex/static/js/main.113451c4.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
